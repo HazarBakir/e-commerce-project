9fa481f811a43dcd2cbeaf0d3ea2ebe82a40df7a
Hazar <92859817+HazarBakir@users.noreply.github.com>
2025-09-25 22:29:02 +0300

    add server proxy config fore /api and /images

--- ecommerce-frontend/vite.config.js
+++ ecommerce-frontend/vite.config.js
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import eslint from 'vite-plugin-eslint'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000'
+      },
+      '/images': {
+        target: 'http://localhost:3000'
+      }
+    }
+  }
+});
